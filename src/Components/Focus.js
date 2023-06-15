0e5031d73fdf17d71b0211f93c10cf51c0485ff7
Konstantinos Agathopoulos <kostaga203@gmail.com>
2023-06-15 19:05:46 +0300

    continuing focus

--- src/Components/Focus.js
+++ src/Components/Focus.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import Menu from './Menu';
-import Container from './Container';
 import '../App.css';
 import '../Focus.css';
 import { Link } from 'react-router-dom';
@@ -14,35 +13,62 @@ const Focus = () => {
 		<div className="App Focus">
 
 			<Menu />
-			<Link to={'/'}>
-			<button className='goBack' type='text'>Go back</button>
-			</Link>
-			<Container>
-
-
-			<div className='warper'>
 			
-				<div className='info_focus'>
-						<div className='name'>
-							<h3 className='id'>n 001</h3>
-							<h3 className='name'>Bulbasaur</h3>
-						</div>
-						<div className='type_focus'>
-							<div className='type_focus type1_focus'>Grass</div>
-							<div className='type_focus type2_focus'>Poison</div>
-						</div>
-				</div>
-					
-				<div  className='pokemon_focus'>
-				<img alt='pokemon' src={Bulb} width={'300px'} height={'300px'}/>
+
+			<div className='container_focus'>
+		
+
+			<div className='main-container'>
+
+				<div className='warper'>
+				
+					<div className='info_focus'>
+							<div className='name'>
+								<h3 className='id'>n 001</h3>
+								<h3 className='name'>Bulbasaur</h3>
+							</div>
+							<div className='type_focus'>
+								<div className='type_focus type1_focus'>Grass</div>
+								<div className='type_focus type2_focus'>Poison</div>
+							</div>
+					</div>
+						
+					<div  className='pokemon_focus'>
+						<img alt='pokemon' src={Bulb} width={'300px'} height={'300px'}/>
+					</div>
+
+					</div>
+
+					<div className='warper'>
+
+					<ul className='details'>
+						<li>Forms</li>
+						<li>Detail</li>
+						<li>Stats</li>
+					</ul>
+
+					<div className='forms'>
+						<img alt='pokemon' src={Bulb} width={'100px'} height={'100px'}/>
+						<img alt='pokemon' src={Bulb} width={'100px'} height={'100px'}/>
+						<img alt='pokemon' src={Bulb} width={'100px'} height={'100px'}/>
+						<img alt='pokemon' src={Bulb} width={'100px'} height={'100px'}/>
+					</div>
+
+					<h2 className='form_name'>Mega Charizard</h2>
+
+					<h3 className='form_description'>The overwhelming power that fills its entire body causes it to turn black and creates intense blue flames.</h3>
+
+					</div>
+
+					<Link to={'/'}>
+					<button className='goBack' type='text'>Go back</button>
+					</Link>
+				
 			</div>
 
+
+
 			</div>
-
-
-
-
-			</Container>
 		</div>
 	);
 }
